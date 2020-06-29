110e06128104500b39177902c19f04d9e5010348
Martasegui2000 <56691646+Martasegui2000@users.noreply.github.com>
2020-06-30 01:24:58 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import MyHeader from './components/MyHeader';
+import MyMain from "./components/MyMain";
+import MyFooter from "./components/MyFooter";
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <MyHeader/>
+        <MyMain/>
+        <MyFooter/>
+    </div>
+  );
+}
+
+export default App;
